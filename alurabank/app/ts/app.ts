0cc306b69274e1db41f3776533a3994797ab8deb
Git da Bru <bruna.reveriego91@gmail.com>
2021-02-02 15:44:53 -0300

    alurabank

---
+++ alurabank/app/ts/app.ts
@@ -0,0 +1,7 @@
+const negociacao = new Negociacao(new Date(),1,100);
+negociacao._quantidade=3;
+
+console.log(negociacao);
+console.log(negociacao.volume);
+console.log(negociacao.quantidade);
+
---
+++ alurabank/app/ts/models/Negociacao.ts
@@ -0,0 +1,52 @@
+class Negociacao {
+
+    private _data;
+    private _quantidade;
+    private _valor;
+
+    constructor(data, quantidade, valor) {
+
+        // if(!data) {
+        //     throw new Error('Data deve ser preenchida');
+        // }
+
+        // if(!quantidade) {
+        //     throw new Error('Quantidade deve ser preenchido');
+        // }
+
+        // if(!valor) {
+        //     throw new Error('Valor deve ser preenchido');
+        // }
+
+
+
+
+
+        //propriedades não podem ser alteradas fora dos métodos da própria classe
+        //utilizar underscore
+
+        this._data = data;
+        this._quantidade = quantidade;
+        this._valor = valor;
+
+    }
+
+    get data() {
+        return this._data;
+    }
+
+  
+    get quantidade() {
+        return this._quantidade;
+    }
+
+
+    get valor() {
+        return this._valor;
+    }
+
+    get volume() {
+        return this._quantidade * this._valor;
+    }
+
+}
